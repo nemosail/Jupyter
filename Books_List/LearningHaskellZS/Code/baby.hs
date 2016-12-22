fce67385580d7cb6b0377200d8f89e5ab4849c64
nemosail_dorm <nemosail@foxmail.com>
2016-12-22 23:29:09 +0800

    keep learning

--- Books_List/LearningHaskellZS/Code/baby.hs
+++ Books_List/LearningHaskellZS/Code/baby.hs
@@ -11,3 +11,13 @@ cannanO'Brien = "It's a-me, Canan O'Brien!"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+lucky :: (Integral a) => a -> String   
+lucky 7 = "LUCKY NUMBER SEVEN!"   
+lucky x = "Sorry, you're out of luck, pal!"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial ( n - 1 )
+
+
