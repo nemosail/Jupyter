d1c484c1e07f13d91e1c54b884c5a6ae8857bdd3
nemosai_laptop <nemosail@foxmail.com>
2016-12-22 19:26:49 +0800

    learn haskell

--- Books_List/LearningHaskellZS/Code/baby.hs
+++ Books_List/LearningHaskellZS/Code/baby.hs
@@ -7,3 +7,7 @@ doubleSmallNumber x = if x > 100
                     else x*2
 
 cannanO'Brien = "It's a-me, Canan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
